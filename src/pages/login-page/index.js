26baba518e9c3249c3cbc8caef181a11b58757c9
Taylog <taylorg198739@gmail.com>
2020-01-23 06:46:41 -0500

    Enter key submit

--- src/pages/login-page/index.js
+++ src/pages/login-page/index.js
@@ -3,6 +3,12 @@ import { navigate } from 'hookrouter'
 
 const LoginPage = () => {
     const [username, setUsername] = useState("");
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleSubmit();    
+        }
+    }
     const handleSubmit = () => {
         if (username) {
             window.localStorage.setItem('username', username);
@@ -12,7 +18,7 @@ const LoginPage = () => {
 
     return (
         <div className="d-flex justify-content-center mt-5">
-            <input value={username} placeholder="username" onChange={(e) => setUsername(e.target.value)}></input>
+            <input value={username} className="input-form" placeholder="username" onKeyDown={handleKeyDown} onChange={(e) => setUsername(e.target.value)}></input>
             <button onClick={handleSubmit}>submit</button>
         </div>
     )
